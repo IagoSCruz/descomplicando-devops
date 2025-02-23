372837a051009058b164d70f804be2485777c4bd
Iago Cruz <iagosilvacontato@gmail.com>
2025-02-23 17:09:24 -0300

    teste

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Subindo aplicação em NodeJS.');
+  res.send('Rodando teste na GCP!');
 });
 
 app.listen(PORT, HOST, () => {
